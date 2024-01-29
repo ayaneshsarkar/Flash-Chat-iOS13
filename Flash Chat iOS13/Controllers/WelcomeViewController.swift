e0822dbb65efd81674f9cff72b9af39253cf3c14
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2024-01-30 01:59:47 +0530

    Working with cocoapods

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,15 +7,18 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        let defaultLabel: String = titleLabel.text!
+        
+        titleLabel.text = defaultLabel
     }
     
 
